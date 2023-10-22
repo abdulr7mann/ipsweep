87d7390e0f81f9efbca2f313890d0fc9c5e1df15
abdulr7mann <51442494+abdulr7mann@users.noreply.github.com>
2023-10-23 00:49:28 +0300

    Update ipsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,100 +1,82 @@
 #!/bin/bash
+
+# Color codes for colored output
 BLUE='\e[1;34m'
 RED='\e[1;91m'
 GREEN='\e[32m'
 BOLD='\e[1m'
-resetStyle='\e[0m'
-scriptName=$(basename "$0")
-function valid_ip()
-{
-    local  ips=$1
-    local  stat=1
+RESET='\e[0m'
 
-    if [[ ${ips} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-        OIFS=$IFS
-        IFS='.'
-        ips=(${ips})
-        IFS=$OIFS
-        [[ ${ips[0]} -le 255 && ${ips[1]} -le 255 \
-            && ${ips[2]} -le 255 && ${ips[3]} -le 255 ]]
+# Validates if the provided IP is valid
+valid_ip() {
+    local ip=$1
+    local stat=1
+
+    # Check if the IP format is correct
+    if [[ $ip =~ ^[0-9]{1,3}(\.[0-9]{1,3}){2}$ ]]; then
+        # Split IP into an array and check if each octet is less than or equal to 255
+        IFS='.' read -r -a ip_segments <<< "$ip"
+        [[ ${ip_segments[0]} -le 255 && ${ip_segments[1]} -le 255 && ${ip_segments[2]} -le 255 ]]
         stat=$?
     fi
+
     return $stat
 }
-if [ "${1}" != "" ]
-then
-    if valid_ip ${1}
-    then
-        stat='good'
-    else
-        echo -e "Invalid IP: ${1} \nusage: run without an argument or with first three octets.\n./${scriptName} 10.10.10\n./${scriptName} 192.168.1"
-        exit
+
+# Main function to execute the script
+main() {
+    local script_name=$(basename "$0")
+
+    # Check if the script is run as root
+    if [ "$EUID" -ne 0 ]; then
+        echo -e "${RED}Please run as root.${RESET}\nExample: sudo ./${script_name} 10.10.10"
+        exit 1
     fi
-fi
-if [ -f iplist.txt ]
-then
-	rm iplist.txt >/dev/null
-fi
-if [ "$EUID" -ne 0 ]
-then
-	echo -e "Please run as root\nsudo ./${scriptName} 10.10.10"
-	exit
-fi
-i="$(hostname -I|cut -d'.' -f1-3)"
-interface="$(ifconfig | grep -v 'lo' | grep ': '|sed 'q1' |cut -d':' -f1)"
-for ip in {1..254}
-do
-	if [ "${1}" = "" ]
-	then
-		ping -c 1 $i.${ip} > ping${ip}.txt && cat ping${ip}.txt|grep -oP 'ttl=\K[^ ]+' >> os.txt && cat ping${ip}.txt|grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> ip.txt &
-		arping -I ${interface} -c 1 $i.${ip} |grep -oP 'y from \K[^ ]+' >> arping.txt &
-	elif [ "${1}" != "" ]
-	then
-		ping -c 1 $1.${ip} > ping${ip}.txt && cat ping${ip}.txt|grep -oP 'ttl=\K[^ ]+' >> os.txt && cat ping${ip}.txt|grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> ip.txt &
-		arping -I ${interface} -c 1 $i.${ip} |grep -oP 'y from \K[^ ]+' >> arping.txt &
-	fi
-done
-wait
-while read os <&4 && read ip <&3
-do
-	if [ "${1}" = "" ]
-	then
-		if [ "${os}" = "64" ]
-		then
-			echo "${ip}" >> iplist.txt
-			echo -e "${BOLD}${ip}${resetStyle} ${GREEN}live${resetStyle} ${RED}Linux${resetStyle}"
-		elif [ "${os}" = "128" ]
-		then
-			echo "${ip}" >> iplist.txt
-			echo -e "${BOLD}${ip}${resetStyle} ${GREEN}live${resetStyle} ${BLUE}Window${resetStyle}"
-		fi
-	elif [ "${1}" != "" ]
-	then
-		if [ "${os}" = "64" ]
-		then
-			echo "${ip}" >> iplist.txt
-			echo -e "${BOLD}${ip}${resetStyle} ${GREEN}live${resetStyle} ${RED}Linux${resetStyle}"
-		elif [ "${os}" = "128" ]
-		then
-			echo "${ip}" >> iplist.txt
-			echo -e "${BOLD}${ip}${resetStyle} ${GREEN}live${resetStyle} ${BLUE}Window${resetStyle}"
-		elif [ "${os}" = "256" ] || [ "${os}" = "255" ] || [ "${os}" = "254" ]
-		then
-			echo "${ip}" >> iplist.txt
-			echo -e "${BOLD}${ip}${resetStyle} ${GREEN}live${resetStyle} ${RED}OpenBSD/Cisco/Oracle${resetStyle}"
-		fi
-	fi
-done 4<os.txt 3<ip.txt
-grep -vf iplist.txt arping.txt > a.txt
-while read arp
-do
-	echo "${arp}" >> iplist.txt
-	echo -e "${BOLD}${arp}${resetStyle} ${GREEN}alive${resetStyle}"
-done <a.txt
-chmod 666 iplist.txt
-echo 'Output -> iplist.txt'
-rm os.txt >/dev/null
-rm ping*.txt >/dev/null
-rm arping*.txt >/dev/null
-rm ip.txt >/dev/null
-rm a.txt >/dev/null
+
+    # If an IP is provided, validate it
+    if [ "$1" ]; then
+        if ! valid_ip "$1"; then
+            echo -e "${RED}Invalid IP: $1${RESET}\nUsage: ./${script_name} [first three octets of IP]\nExample: ./${script_name} 192.168.1"
+            exit 1
+        fi
+    fi
+
+    local base_ip=${1:-$(hostname -I | cut -d'.' -f1-3)}
+    local interface=$(ifconfig | grep -v 'lo' | cut -d':' -f1 | head -n1)
+    
+    > iplist.txt
+
+    for ip in $(seq 1 254); do
+        (
+            if ping -c 1 ${base_ip}.${ip} &> /dev/null; then
+                ttl=$(ping -c 1 ${base_ip}.${ip} | grep 'ttl=' | grep -oP 'ttl=\K[^ ]+')
+                
+                case $ttl in
+                    64) os="Linux" ;;
+                    128) os="Windows" ;;
+                    255|256|254) os="OpenBSD/Cisco/Oracle" ;;
+                    *) os="Unknown OS" ;;
+                esac
+
+                echo "${base_ip}.${ip} ${os}" >> iplist.txt
+            fi
+        ) &
+    done
+    
+    wait
+
+    # Print out the results in a cleaner format
+    cat iplist.txt | while read -r ip os; do
+        case $os in
+            Linux) echo -e "${BOLD}${ip}${RESET} ${GREEN}live${RESET} ${RED}Linux${RESET}" ;;
+            Windows) echo -e "${BOLD}${ip}${RESET} ${GREEN}live${RESET} ${BLUE}Windows${RESET}" ;;
+            OpenBSD/Cisco/Oracle) echo -e "${BOLD}${ip}${RESET} ${GREEN}live${RESET} ${RED}OpenBSD/Cisco/Oracle${RESET}" ;;
+            *) echo -e "${BOLD}${ip}${RESET} ${GREEN}live${RESET} ${BLUE}Unknown OS${RESET}" ;;
+        esac
+    done
+
+    echo 'Output -> iplist.txt'
+}
+
+# Execute the main function with all arguments passed
+main "$@"

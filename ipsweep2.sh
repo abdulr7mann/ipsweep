8bfe9f9ae5e7428499e5d2439dc4ce25c87477ab
abdulr7mann <51442494+abdulr7mann@users.noreply.github.com>
2023-10-23 00:53:09 +0300

    Update ipsweep2.sh

--- ipsweep2.sh
+++ ipsweep2.sh
@@ -1,24 +1,56 @@
 #!/bin/bash
+
+# Color codes for colored output
 RED='\e[1;91m'
 GREEN='\e[32m'
-resetStyle='\e[0m'
-for ip in {1..254}
-do
-	results=$(ping -c 1 $1.$ip|grep ttl)
-	live=$(echo $results |cut -d' ' -f1)
-	os=$(echo $results |grep -oP 'ttl=\K[^ ]+')
-	if [ ! -z "$live" ] && ( [ "$os" = "30" ] || [ "$os" = "60" ] || [ "$os" = "64" ] )
-	then
-		echo -e "${BOLD}${1}.${ip}${resetStyle} ${GREEN}live${resetStyle} ${RED}Linux${resetStyle}"
-	elif [ ! -z "$live" ] && ( [ "$os" = "128" ] || [ "$os" = "127" ] )
-	then
-		echo -e "${BOLD}${1}.${ip}${resetStyle} ${GREEN}live${resetStyle} ${RED}Windows${resetStyle}"
-	elif [ ! -z "$live" ] && ( [ "${os}" = "256" ] || [ "${os}" = "255" ] || [ "${os}" = "254" ] )
-	then
-	      echo -e "${BOLD}${1}.${ip}${resetStyle} ${GREEN}live${resetStyle} ${RED}OpenBSD/Cisco/Oracle${resetStyle}"
-	elif [ ! -z "$live" ]
-	then
-		echo -e "${BOLD}${1}.${ip}${resetStyle} ${GREEN}live${resetStyle} Unknown"
-	fi
-done
-# Abdulrahman
+BOLD='\e[1m'
+RESET='\e[0m'
+
+# Validates the IP prefix
+validate_ip_prefix() {
+    if [[ ! $1 =~ ^[0-9]{1,3}(\.[0-9]{1,3}){2}$ ]]; then
+        echo -e "${RED}Invalid IP prefix.${RESET}"
+        echo "Usage: $0 [IP_PREFIX]"
+        echo "Example: $0 192.168.1"
+        exit 1
+    fi
+}
+
+# Main function to perform the ping and output results
+ping_hosts() {
+    local ip_prefix=$1
+
+    for ip in $(seq 1 254); do
+        local result=$(ping -c 1 ${ip_prefix}.${ip} 2>/dev/null | grep ttl)
+        
+        if [ -n "$result" ]; then
+            local ttl=$(echo $result | grep -oP 'ttl=\K[^ ]+')
+            local os
+
+            case $ttl in
+                30|60|64) os="${RED}Linux${RESET}" ;;
+                127|128) os="${RED}Windows${RESET}" ;;
+                254|255|256) os="${RED}OpenBSD/Cisco/Oracle${RESET}" ;;
+                *) os="Unknown" ;;
+            esac
+
+            echo -e "${BOLD}${ip_prefix}.${ip}${RESET} ${GREEN}live${RESET} $os"
+        fi
+    done
+}
+
+# Entry point of the script
+main() {
+    if [ -z "$1" ]; then
+        echo -e "${RED}IP prefix is required.${RESET}"
+        echo "Usage: $0 [IP_PREFIX]"
+        echo "Example: $0 192.168.1"
+        exit 1
+    fi
+
+    validate_ip_prefix "$1"
+    ping_hosts "$1"
+}
+
+# Execute the main function with all arguments passed
+main "$@"
